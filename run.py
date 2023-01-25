bcd1e00ee85717864e1c7ffd3ebba89463f4b6a1
DannyGilligan <gilligan.danny@gmail.com>
2023-01-25 22:23:01 +0000

    Fix formatting of inline comments

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
@@ -7,30 +6,29 @@ HEADER = "\n___________________________________BATTLESHIP_______________________
 
 WELCOME_MESSAGE = "Beyond Top Secret: For Danny's eyes only.\n\nYour mission is to intercept and destroy a fleet of 5 enemy Destroyers \nthat are currently in pursuit of 2 friendly Merchant ships sailing for our \nNorthern Port. These Merchant ships are on a clandestine mission to deliver \nclassified cargo that will turn the tide of this war once and for all!\n\nUnfortunately, during a recent skirmish, the Merchant ships lost all \ncommunication capabilities and the Enemy's radar jamming technology is \npreventing us from locating them. But we know they're out there somewhere.\n\nHunt down the enemy with extreme prejudice. Avoid friendly fire at all costs. \n\nRescue the Merchant Ships. Losing their cargo, means losing the war!\n"
 
-DEFAULT_SYMBOL = "~"                            #Defualt symbol (as close to a wave as I could find!)
-ENEMY_HIT_SYMBOL = "X"                          #Symbol to denote a hit on an enemy ship
-MISS_SYMBOL = "O"                               #Symbol to denote a miss
-MERCHANT_HIT_SYMBOL = "M"                       #Symbol to denote a hit on a friendly merchant ship
+DEFAULT_SYMBOL = "~"                            # Defualt symbol (as close to a wave as I could find!)
+ENEMY_HIT_SYMBOL = "X"                          # Symbol to denote a hit on an enemy ship
+MISS_SYMBOL = "O"                               # Symbol to denote a miss
+MERCHANT_HIT_SYMBOL = "M"                       # Symbol to denote a hit on a friendly merchant ship
 
-ROWS = 8                                        #Y axis (the rows will be counted vertically)
-COLS = 8                                        #X axis (the columns will be counted horizontally)
+ROWS = 8                                        # Y axis (the rows will be counted vertically)
+COLS = 8                                        # X axis (the columns will be counted horizontally)
 
-GAME_BOARD = []                                 #Empty list to hold the list of lists that will form the game board
+GAME_BOARD = []                                 # Empty list to hold the list of lists that will form the game board
 
-for row in range(ROWS):                         #For loop to generate rows on the game board based on the ROWS value
-    GAME_BOARD.append([DEFAULT_SYMBOL] * COLS)  #Code to create the 'column' elements within each row array by appending a '~' symbol as a placeholder multiplied by the COLS value
+for row in range(ROWS):                         # For loop to generate rows on the game board based on the ROWS value
+    GAME_BOARD.append([DEFAULT_SYMBOL] * COLS)  # Code to create the 'column' elements within each row array by appending a '~' symbol as a placeholder multiplied by the COLS value
 
 def print_game_board():
     """
     This function will generate a dynamically sized game board based on the number of rows and columns determined by the user's input
     """
     print("\n---------------------- Battleship Command Operations Deck ----------------------\n\nAmmunition Remaining = \n\n[O] Misses =\n[X] Enemy Ships Hit = X     (*5* Remaining)\n[M] Merchant Ships Hit = M  (*2* Remaining)\n")
-    col_headers = []                             #Empty array to hold the column header values based on the userinput
-    for i in range(COLS):                        #Iterates based on the range dictated by the user input  
- 
-        col_headers.append(i)                    #Appends the column header numbers to the array 
-    col_headers.insert(0," ")                    #NOTE FOR BUG, HAD TO INDENT THIS OUTSIDE OF THE LOOP, and insert a blank space so it would align
-    print(*col_headers, sep=' ')                 #Breaks out the column headers from the array and prints horizontally
+    col_headers = []                             # Empty array to hold the column header values based on the userinput
+    for i in range(COLS):                        # Iterates based on the range dictated by the user input  
+        col_headers.append(i)                    # Appends the column header numbers to the array 
+    col_headers.insert(0," ")                    # NOTE FOR BUG, HAD TO INDENT THIS OUTSIDE OF THE LOOP, and insert a blank space so it would align
+    print(*col_headers, sep=' ')                 # Breaks out the column headers from the array and prints horizontally
 
     row_counter = 0
     for row_array in GAME_BOARD: 
@@ -40,4 +38,6 @@ def print_game_board():
             print(col_elem, end=" ")
         print()
 
+
+print(WELCOME_MESSAGE)
 print_game_board()
