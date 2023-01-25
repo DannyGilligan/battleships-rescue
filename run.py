dcc106c3eae0be4b62f29235b77f38a752c5b15a
DannyGilligan <gilligan.danny@gmail.com>
2023-01-25 21:48:53 +0000

    Add symbol placeholders to denote default, enemy hit, miss or merchant hit

--- run.py
+++ run.py
@@ -7,3 +7,9 @@ HEADER = "\n___________________________________BATTLESHIP_______________________
 
 WELCOME_MESSAGE = "Beyond Top Secret: For Danny's eyes only.\n\nYour mission is to intercept and destroy a fleet of 5 enemy Destroyers \nthat are currently in pursuit of 2 friendly Merchant ships sailing for our \nNorthern Port. These Merchant ships are on a clandestine mission to deliver \nclassified cargo that will turn the tide of this war once and for all!\n\nUnfortunately, during a recent skirmish, the Merchant ships lost all \ncommunication capabilities and the Enemy's radar jamming technology is \npreventing us from locating them. But we know they're out there somewhere.\n\nHunt down the enemy with extreme prejudice. Avoid friendly fire at all costs. \n\nRescue the Merchant Ships. Losing their cargo, means losing the war!\n"
 
+DEFAULT_SYMBOL = "~"                    #Defualt symbol (as close to a wave as I could find!)
+ENEMY_HIT_SYMBOL = "X"                  #Symbol to denote a hit on an enemy ship
+MISS_SYMBOL = "O"                       #Symbol to denote a miss
+MERCHANT_HIT_SYMBOL = "M"               #Symbol to denote a hit on a friendly merchant ship
+
+
