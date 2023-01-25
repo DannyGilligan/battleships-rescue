d6384eeddfbdb36ad9acbc5babc6b5ee3b3bbd9d
DannyGilligan <gilligan.danny@gmail.com>
2023-01-25 21:46:19 +0000

    Add HEADER and WELCOM_MESSAGE text to be displayed to user

--- run.py
+++ run.py
@@ -5,3 +5,5 @@
 
 HEADER = "\n___________________________________BATTLESHIP___________________________________\n_____________________________________RESCUE_____________________________________\n"
 
+WELCOME_MESSAGE = "Beyond Top Secret: For Danny's eyes only.\n\nYour mission is to intercept and destroy a fleet of 5 enemy Destroyers \nthat are currently in pursuit of 2 friendly Merchant ships sailing for our \nNorthern Port. These Merchant ships are on a clandestine mission to deliver \nclassified cargo that will turn the tide of this war once and for all!\n\nUnfortunately, during a recent skirmish, the Merchant ships lost all \ncommunication capabilities and the Enemy's radar jamming technology is \npreventing us from locating them. But we know they're out there somewhere.\n\nHunt down the enemy with extreme prejudice. Avoid friendly fire at all costs. \n\nRescue the Merchant Ships. Losing their cargo, means losing the war!\n"
+
