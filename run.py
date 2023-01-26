5428139c1ed562e04b00bfae6a409e02cf72b2ce
DannyGilligan <gilligan.danny@gmail.com>
2023-01-26 21:25:29 +0000

    Add temporary code to demonstrate progression leading up to display of the game board to user

--- run.py
+++ run.py
@@ -3,19 +3,20 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from random import randint
 import time
+import os
 
 
-HEADER = "\n___________________________________BATTLESHIP___________________________________\n_____________________________________RESCUE_____________________________________\n"
+HEADER = "\n___________________________________BATTLESHIP___________________________________\n_____________________________________RESCUE_____________________________________\n_______________________________________XX_______________________________________\n"
 
-WELCOME_MESSAGE = "Beyond Top Secret: For Danny's eyes only.\n\nYour mission is to intercept and destroy a fleet of 5 enemy Destroyers \nthat are currently in pursuit of 2 friendly Merchant ships sailing for our \nNorthern Port. These Merchant ships are on a clandestine mission to deliver \nclassified cargo that will turn the tide of this war once and for all!\n\nUnfortunately, during a recent skirmish, the Merchant ships lost all \ncommunication capabilities and the Enemy's radar jamming technology is \npreventing us from locating them. But we know they're out there somewhere.\n\nHunt down the enemy with extreme prejudice. Avoid friendly fire at all costs. \n\nRescue the Merchant Ships. Losing their cargo, means losing the war!\n"
+
 
 DEFAULT_SYMBOL = "~"           # Defualt symbol
 ENEMY_HIT_SYMBOL = "X"         # Hit on an enemy ship
 MISS_SYMBOL = "O"              # Denotes a miss
 MERCHANT_HIT_SYMBOL = "M"      # Denotes hit on friendly merchant ship
 
-ROWS = 8            # Y axis (the rows will be counted vertically)
-COLS = 8            # X axis (the columns will be counted horizontally)
+ROWS = 7            # Y axis (the rows will be counted vertically)
+COLS = 7            # X axis (the columns will be counted horizontally)
 
 GAME_BOARD = []     # Empty list to hold the list of lists for board
 
@@ -26,7 +27,8 @@ def print_game_board():
     """
     This function will generate a dynamically sized game board based on the number of rows and columns determined by the user's input
     """
-    print("---------------------- Battleship Command Operations Deck ----------------------\n\nAmmunition Remaining = \n\n[O] Misses =\n[X] Enemy Ships Hit = X     (*5* Remaining)\n[M] Merchant Ships Hit = M  (*2* Remaining)\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("---------------------- Battleship Command Operations Deck ----------------------\n\nAmmunition Remaining   = \n\n[O] Misses             = O\n[X] Enemy Ships Hit    = X  (*5* Remaining)\n[M] Merchant Ships Hit = M  (*2* Remaining)\n")
     col_headers = []                             # Empty array to hold the column header values based on the userinput
     for i in range(COLS):                        # Iterates based on the range dictated by the user input  
         col_headers.append(i)                    # Appends the column header numbers to the array 
@@ -60,15 +62,31 @@ enemy_ship_locations = [ship_locations[0], ship_locations[1], ship_locations[2],
 merchant_ship_locations = [ship_locations[5], ship_locations[6]]        # The Merchant ships are retrieved from the last two elements in the ship_locations array
 
 
+# Code below here has been added to outline the pages leading up to displaying the game board to the user
+# Project will be deployed in an unfinished state due to time constraints
+# This game is not currently playable. These lines of code are temporary. The project will continue to be developed in order to resubmit.
 
-print(merchant_ship_locations)
-test_coordinates = '8,5'
+time.sleep(1)
+print(HEADER)
 
-def test():
-    if ship_locations[5] in merchant_ship_locations:
-        print('SUCCESS!')
-    else:
-        print('FAIL')
-
-test()
-
+name = input("\n\n\n\n                                Enter Username:\n")
+MISSION_MESSAGE = f"                  Above Top Secret: For {name}'s eyes only.\n\nYour mission is to intercept and destroy a fleet of 5 enemy Destroyers \nthat are currently in pursuit of 2 friendly Merchant ships sailing for our \nNorthern Port. These Merchant ships are on a clandestine mission to deliver \nclassified cargo that will turn the tide of this war once and for all!\n\nUnfortunately, during a recent skirmish, the Merchant ships lost all \ncommunication capabilities and the Enemy's radar jamming technology is \npreventing us from locating them. But we know they're out there, somewhere.\n\nHunt down the enemy with extreme prejudice. Avoid friendly fire at all costs. \n\nRescue the Merchant Ships. Losing that cargo, means losing the war!\n"
+time.sleep(.5)
+print('                               Username accepted')
+print('\n\n                           Retreiving Mission Details\n\n')
+time.sleep(1.5)
+os.system('cls' if os.name == 'nt' else 'clear')
+time.sleep(.5)
+print(HEADER)
+print(MISSION_MESSAGE)
+mission_accept = input("\n                              Accept Mission? Y/N\n")
+time.sleep(.5)
+os.system('cls' if os.name == 'nt' else 'clear')
+print_game_board()
+column_fire_upon = input('\nSELECT COLUMN TO FIRE UPON:\n')
+row_fire_upon = input('SELECT ROW TO FIRE UPON:\n')
+column_fire_coordinate = column_fire_upon
+row_fire_coordinate = row_fire_upon
+time.sleep(.5)
+fire_upon_status = ('                    **** YOU SUNK AN ENEMY DESTROYER ****')
+print(fire_upon_status)
